6238fe1e64dd329e7ef4c2cb720213a96565ef15
tsomielvis <elvis.muthomi@studentmoringaschool.com>
2024-07-16 20:56:28 +0300

    latest

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function writeCards(names, event) {
-    let messages = [];
-    for (let i = 0; i < names.length; i++) {
-      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-    }
-    return messages;
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
-  
-  function countDown(number) {
-    while (number >= 0) {
-      console.log(number);
-      number--;
-    }
+  return messages;
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
   }
+}
+
 
